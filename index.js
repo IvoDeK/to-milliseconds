a07445023b8f14a1c1a6e0388865cfce3dcd0a3b
Unknown <ivodoetyt@gmail.com>
2020-03-20 09:54:17 +0100

    Added years, months and weeks to the converter
    This is so you can easily convert from the youtube api since they also give you the years months and weeks variables

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const converters = {
+	years: value => value * 3154e+10,
+	months: value => value * 2628e+9,
+	weeks: value => value * 6048e+8,
 	days: value => value * 864e5,
 	hours: value => value * 36e5,
 	minutes: value => value * 6e4,
